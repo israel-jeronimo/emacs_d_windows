0a794337ba283f39e29d12af68da7f8a54e2f230
ijero_pc <informatikillo@gmail.com>
2024-09-06 15:01:57 -0600

    Updated magit config. Setting for autocomplete mode.

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(misterioso))
- '(package-selected-packages '(magit)))
+ '(package-selected-packages '(auto-complete magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,3 +15,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
+
+;; Emacs auto-complete-mode at startup
+;; https://stackoverflow.com/questions/8095715/emacs-auto-complete-mode-at-startup
+(setq ac-modes '(org-mode))
+(global-auto-complete-mode t)
