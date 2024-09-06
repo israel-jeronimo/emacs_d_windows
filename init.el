02749da1ff7d9d8231445818b675b95357ae69bd
ijero_pc <informatikillo@gmail.com>
2024-09-06 12:36:09 -0600

    Set init for installing from Melpa. It's used to install magit.

--- init.el
+++ init.el
@@ -3,10 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(misterioso)))
+ '(custom-enabled-themes '(misterioso))
+ '(package-selected-packages '(magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight regular :height 158 :width normal)))))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
